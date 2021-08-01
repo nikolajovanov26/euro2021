6c3ed585c939213fb218af84a0f8da8a940fe1f1
nikolajovanov26 <nikolajovanov26@gmail.com>
2021-08-01 15:10:03 +0200

    bootstrap

--- index.php
+++ index.php
@@ -1,2 +1,8 @@
+<?php include 'view/header.php' ?>
+
 <h1>Euro 2021</h1>
+<p> test </p>
+<h2>t</h2>
 <?php echo 'TEST'; ?>
+
+<?php include 'view/footer.php' ?>
---
+++ view/header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<meta charset="UTF-8">
+
+<title>Euro 2021</title>
+
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<link rel="stylesheet" href="../bootstrap/css/bootstrap.css">
+
+
+<!--<link rel="stylesheet" href="">
+<script src=""></script> -->
+
+<body>
