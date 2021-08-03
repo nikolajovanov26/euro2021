10a6af9999db0ece9ad2e954cea756975349992f
nikolajovanov26 <nikolajovanov26@gmail.com>
2021-08-03 15:35:14 +0200

    homepage

--- index.php
+++ index.php
@@ -2,27 +2,48 @@
 <?php include 'view/navbar.php' ?>
 
 
-<div class="container pad-bot">
+<div class="container pad-top">
     <div class="row">
-        <div class="col-8">
-            <h1>Euro 2021</h1>
-            <p> test </p>
-            <h2>t</h2>
-            <?php echo 'TEST'; ?>
+        <div class="col">
+            <img class="pp" src="images/pp.png" width="200" class="pp">
         </div>
-        <div class="col-4">
-            <figure class="figure">
-                <img src="images/euro_logo.jpg" width="500" />
-                <figcaption class="figure-caption">Euro 2020 Logo</figcaption>
-            </figure>
-            <div class="progress">
-                <div class="progress-bar progress-bar-striped" role="progressbar" style="width: 5%" aria-valuenow="5" aria-valuemin="0" aria-valuemax="100"></div>
+        <div class="col-6 div-bottom">
+            <div class="progres-text">
+                Your progress:
+                <span style="color:#2C5364">55</span>/100
             </div>
+            <div class="progress">
+                <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="55" aria-valuemin="0" aria-valuemax="100" style="width: 55%"></div>
+            </div>
+        </div>
+        <div class="col">
+        
         </div>
     </div>
 </div>
-
-
+<div class="whitespace-30"></div>
+<div class="container pad-top">
+    <div class="row">
+        <div class="col-sm cards">
+            <a href="profile.php">
+                <img src="images/profile.png" width="200">
+                <p>Profile</p>
+            </a>
+        </div>
+        <div class="col-sm cards">
+            <a href="stickers.php">
+                <img src="images/stickers.png" width="200">
+                <p>Stickers</p>
+            </a>
+        </div>
+        <div class="col-sm cards">
+            <a href="album.php">
+                <img src="images/album.png" width="200">
+                <p>Album</p>
+            </a>
+        </div>
+    </div>
+</div>
 
 
 <?php include 'view/footer.php' ?>
--- view/navbar.php
+++ view/navbar.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-lg navbar-dark nav-bg">
-    <a class="navbar-brand" href="../index.php"><img src="images/logo_white.png" width="120"></a>
+    <a class="navbar-brand" href="index.php"><img src="images/logo_white.png" width="120"></a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
@@ -9,10 +9,10 @@
             <a class="nav-link" href="index.php">Home</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" href="album.php">My Album</a>
+            <a class="nav-link" href="stickers.php">Stickers</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" href="stickers.php">Stickers</a>
+            <a class="nav-link" href="album.php">My Album</a>
         </li>
         <li class="nav-item">
             <a class="nav-link" href="info.php">Instructions</a>
